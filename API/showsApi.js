4c7261b602799e8b43acd92c5b8bd0ffa6689d97
Gabriel Jefferson <phantom8528@gmail.com>
2022-05-17 11:28:45 -0400

    gj-api-ticket001: Created js files for server with routes and a blank file for api fetches

---
+++ API/showsApi.js
@@ -0,0 +1,7 @@
+/**
+ * Fetch Api for all of the tv shows that we're building
+ * --------------------------------------------------------------
+ * SOURCE: Imdb-api.com
+ * [WARNING]: If api key isn't inside of a .gitignore file, please remove it from this file BEFORE PUSHING UP TO GITHUB!!!
+ */
+

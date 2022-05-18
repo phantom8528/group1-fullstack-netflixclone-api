71f140246018db19dd45ec929f85a47e7db76a97
Gabriel Jefferson <phantom8528@gmail.com>
2022-05-18 10:53:18 -0400

    gj-api-ticket002: Connected server to db, created post for sign-up route, needs to be tested with its react counterpart

--- API/seed.sql
+++ API/seed.sql
@@ -1,7 +1,28 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS shows (
+    id SERIAL PRIMARY KEY,
+    show_name VARCHAR(255),
+    show_description VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS episodes (
+    ep_id SERIAL PRIMARY KEY,
+    ep_season INTEGER,
+    ep_title VARCHAR(50),
+    ep_desc VARCHAR(300)
+
+);
+
+
+
+
+
+
+
